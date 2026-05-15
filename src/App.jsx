603348849d2032ae4dee7862084d25f6de3283d1
shehan kaushalya <shehankaushalyadurage@gmail.com>
2026-05-15 15:35:52 +0530

    Initial Commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Simple React App</h1>
+        <p>This project is ready to run with Vite.</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
